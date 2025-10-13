08e9a15ded2fffc7ecf376774680e95d92ca48eb
Lord Rajkumar <rajphx299@gmail.com>
2025-10-13 17:48:10 -0400

    Added rb_read and driver fucker

--- src/ring_buffer_driver.c
+++ src/ring_buffer_driver.c
@@ -1,7 +1,10 @@
 #include "ring_buffer_driver.h"
 #include <string.h>
 #include <stdlib.h>
- #include <stdio.h>
+#include <stdio.h>
+
+#define NDEBUG
+#include <assert.h>
 
 struct ring_buffer
 {
@@ -22,6 +25,7 @@ ring_buffer_t * rb_create(size_t capacity){
     if(!buffer){
         free(rb) ;
         rb = NULL ;
+        // assert(rb==NULL) ;
         perror("malloc") ;
         return NULL ;
     }
@@ -45,28 +49,48 @@ void rb_destroy(ring_buffer_t ** rb){
 }
 
 int rb_write(ring_buffer_t * rb , const void * data , size_t len){
-    /**
-     * TODO : 
-     */
+    if((rb->capacity - rb->count ) >= len && len>0){
+        if(len>rb->capacity-rb->write){
+            memcpy(rb->buffer+rb->write,data ,rb->capacity-rb->write) ;
+            memcpy(rb->buffer ,data + rb->capacity - rb->write, len-(rb->capacity-rb->write) ) ;
+        }else{
+            memcpy(rb->buffer+rb->write, data , len) ;}
+        rb->write = (rb->write + len) % rb->capacity ;
+        rb->count += len ;
+        return len ;
+    }
+    perror("Fatal : rb_write") ;
     return 0 ;
+}
+
+int rb_read(ring_buffer_t *rb , void * data ,size_t len) {
+    if( len > 0 ){
+        len = (len > rb->count) ? rb->count : len;
+        if((rb->capacity-rb->read )>= len ){
+            memcpy (data , rb->buffer + rb->read , len);
+            rb->read = (rb->read + len) % rb->capacity ;
+        }else{
+            memcpy (data , rb->buffer + rb->read , rb->capacity - rb->read) ;
+            memcpy (data + (rb->capacity - rb->read) , rb->buffer , len - (rb->capacity -rb->read)) ;
+            rb->read = len - (rb->capacity - rb->read);
+        }
+        rb->count -= len ;
+        return len ;
+    }
+    perror("Fatal : rb_read") ;
+    return -1 ;
 }
 
 size_t rb_available(ring_buffer_t * rb){
-    /**
-     * TODO : 
-     */
-    return 0 ;
+    
+    return rb->count ;
 }
 
 size_t rb_free_space(ring_buffer_t * rb){
-    /**
-     * TODO : 
-     */
-    return 0 ;
+    return rb->capacity - rb->count;
 }
 
 void rb_clear(ring_buffer_t * rb) {
-    /**
-     * TODO : 
-     */
+    memset(rb->buffer ,0 , rb->capacity);
+    rb->read = rb->write = rb->count = 0 ;
 }
